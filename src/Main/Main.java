b0085f8ca09a68ddfa5ba448e17fbb9365f89d62
jzho987 <79814775+jzho987@users.noreply.github.com>
2023-10-21 12:10:23 +1300

    feat: setup console app
    setup the console app side of the program

---
+++ src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import Console.ConsoleApp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConsoleApp app = new ConsoleApp();
+        app.start();
+    }
+
+}
